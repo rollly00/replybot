12d2f9d5db389a2de8f46c7c439c0d244e77f54f
Rollly <31448610+rollly00@users.noreply.github.com>
2019-08-30 17:23:13 +0600

    Update main.py

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ import telebot
 import logging
 
 bot = telebot.TeleBot("TOKEN")
-ids = [1234567]
+ids = [1234567] # allowed ids to use bot
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
 	username = message.from_user.username
 	first = str(message.from_user.first_name) + " " + str(message.from_user.last_name)
 	
 	if message.chat.id in ids:
-		bot.send_message(*CHATID*,message.text + "\nFrom:@" + username + "\n" + first)
+		bot.send_message(CHANNELID HERE,message.text + "\nFrom:@" + username + "\n" + first)
 	else:
 		pass
 
